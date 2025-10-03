50d4635b99d99dcc55c80d2206a58a574fe43cab
kaelzubs <donmart4u@gmail.com>
2025-10-03 15:14:19 +0100

    deployment wsgi

--- gameshopa/wsgi.py
+++ gameshopa/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshopa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshopa.settings.production')
 
 application = get_wsgi_application()
