2435aebee9d29955ff1915baca824773ea52e010
kaelzubs <donmart4u@gmail.com>
2025-10-06 12:07:35 +0100

    production deploy1

--- gameshop/urls.py
+++ gameshop/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from core import views
 from django.contrib.sitemaps.views import sitemap
 from .views import StaticViewSitemap
@@ -23,6 +23,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.png'), permanent=True)),
     path("ads.txt", RedirectView.as_view(url=staticfiles_storage.url("ads.txt")))
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
 
 if settings:
