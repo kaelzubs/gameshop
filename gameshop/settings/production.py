19207e1f17a97b8d358f12c052b6df9b4265e7df
kaelzubs <donmart4u@gmail.com>
2025-10-06 17:39:37 +0100

    production deploy4

--- gameshop/settings/production.py
+++ gameshop/settings/production.py
@@ -1,18 +1,6 @@
 from .base import *
 
 DEBUG = config('DEBUG', cast=bool)
-if DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_REFERRER_POLICY = "same-origin"
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    X_FRAME_OPTIONS = 'DENY'
-        
-
 ALLOWED_HOSTS = ['gameshop.onrender.com', '*']
 
 AUTH_PASSWORD_VALIDATORS = [
