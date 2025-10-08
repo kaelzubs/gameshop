5743f099346d57b2695efe3ff400048cf24bed5f
kaelzubs <donmart4u@gmail.com>
2025-10-08 20:23:32 +0100

    production deploy 8

--- gameshop/settings/base.py
+++ gameshop/settings/base.py
@@ -73,12 +73,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
     
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Auth
