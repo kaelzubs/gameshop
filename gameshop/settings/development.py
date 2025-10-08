3e1f9d0968a65fe28059f32d42184aa53c5eeba7
kaelzubs <donmart4u@gmail.com>
2025-10-08 20:18:36 +0100

    production deploy 8

--- gameshop/settings/development.py
+++ gameshop/settings/development.py
@@ -3,6 +3,15 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '*']
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- gameshop/settings/production.py
+++ gameshop/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 from decouple import config
+import dj_database_url
 
 DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = ['gameshop.onrender.com', 'gameshop-9nk6.onrender.com']
@@ -11,33 +12,22 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
 ]
 
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "storages.backends.s3.S3Storage",
-#     },
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT')
+#     }
 # }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-# STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
-    }
+    'default': dj_database_url.config(
+        default='postgresql://gameshop_rcll_user:jIHt3ZWcPkGGpbJCDrr8hFQcMiDYTULD@dpg-d3hn07e3jp1c73fm41j0-a.oregon-postgres.render.com/gameshop_rcll'
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
