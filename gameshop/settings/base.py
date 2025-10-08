d9d3b38d34967d91ac3b3387257fbf5c087a6e7b
kaelzubs <donmart4u@gmail.com>
2025-10-08 21:48:49 +0100

    production deploy 9

--- gameshop/settings/base.py
+++ gameshop/settings/base.py
@@ -78,9 +78,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 
 # Auth
 AUTHENTICATION_BACKENDS = (
