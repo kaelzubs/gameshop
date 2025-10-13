6411a90f68da81266714f25fba9ab4608b428328
kaelzubs <donmart4u@gmail.com>
2025-10-13 07:20:55 +0100

    Morning

--- gameshop/wsgi.py
+++ gameshop/wsgi.py
@@ -6,6 +6,6 @@ from django.core.wsgi import get_wsgi_application
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshop.settings')
 
 # For production use:
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshop.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameshop.settings')
 
 application = get_wsgi_application()
