77cdb3acac71bfdd1ffa711c564b868450b78b5f
kaelzubs <donmart4u@gmail.com>
2025-10-13 02:00:31 +0100

    Midnight

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -82,8 +82,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_env')]
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media_env')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
