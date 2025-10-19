c9cdcd535e77c66a3a0edc3681007ba994996c7f
kaelzubs <donmart4u@gmail.com>
2025-10-20 00:15:23 +0100

    Deploy

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = key
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG == True:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
