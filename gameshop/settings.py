f192b3fa07346436e5940788c53e93b378748629
kaelzubs <donmart4u@gmail.com>
2025-10-20 01:42:26 +0100

    Deploy

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -109,7 +109,11 @@ if DEBUG == True:
 elif DEBUG == False:
     DATABASE_URL = config('DATABASE_URL')
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {'default': {
+    dj_database_url.config(default=DATABASE_URL),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }}
+
 
 # Database
 # if DEBUG == False:
