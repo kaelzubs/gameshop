db157b282262167f9d229919f5ebc2e7cdb66530
kaelzubs <donmart4u@gmail.com>
2025-10-20 06:27:32 +0100

    Deploy Monday

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -108,7 +108,7 @@ DATABASE_URL = config('DATABASE_URL')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'appliku.sqlite3'),
     }
 }
 
