58a6900a8f1b7b81e6b9e48bc3c3563f2eaad8b1
kaelzubs <donmart4u@gmail.com>
2025-10-20 09:54:11 +0100

    Deploy Monday

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -11,7 +11,8 @@ key = "".join(secrets.choice(choices) for n in range(100))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = key
+# SECRET_KEY = key
+SECRET_KEY = config('SECRET_KEY', default=key)
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
